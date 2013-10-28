bf1503587fea891a5bc2eab531d1d85319bfa0b3
John Dalton <john.dalton@neuf.fr>
2013-10-28 09:40:04 +0100

    Production update

--- application/configs/tradecapture_update_1.sql
+++
@@ -1,2 +0,0 @@
-alter table tradecapture.`trade` add column `not_amt_capped` bool after `not_amount_1`;
-alter table tradecapture.`trade_archive` add column `not_amt_capped` bool after `not_amount_1`;
